6b30bd44d1553278b8d6177475072adcb1014b68
hwelsters <84760072+hwelsters@users.noreply.github.com>
2023-02-27 00:54:11 -0700

    Date logging

--- src/sleepyask/chat.py
+++ src/sleepyask/chat.py
@@ -3,7 +3,7 @@ import sleepyask.chatgpt
 import traceback
 import logging
 
-def sleepy_ask(config, questions : list, output_file_path : str):
+def sleepy_ask(config, questions : list, output_file_path : str, verbose : bool=False):
     '''
     `config` should contain your ChatGPT email and password.\n
     `questions` should contain a list of questions you would like ChatGPT to answer.\n
@@ -11,9 +11,11 @@ def sleepy_ask(config, questions : list, output_file_path : str):
     '''
     if not isinstance(questions, list): raise ValueError("[questions] should be a list")
     def func() -> None:
-        try: sleepyask.chatgpt.ask_all_questions(config=config, questions=questions, output_file_path=output_file_path)
-        except Exception as e: 
-            logging.error(traceback.format_exc())
+        try: sleepyask.chatgpt.ask_all_questions(config=config, questions=questions, output_file_path=output_file_path, verbose=verbose)
+        except Exception as e: logging.error(traceback.format_exc())
+    
     schedule.every(1).hours.do(func)
+    schedule.every(1).hours.do(func)
+
     func()
     while True: schedule.run_pending()
--- src/sleepyask/chatgpt.py
+++ src/sleepyask/chatgpt.py
@@ -1,6 +1,8 @@
 from revChatGPT.V1 import Chatbot
 import json
 from pathlib import Path
+import logging
+from datetime import datetime
 
 def __append_to_file(output_file_path: str, data):
     with open(output_file_path, 'a') as outfile:
@@ -13,16 +15,26 @@ def __clean_str_for_json(text: str):
     return text.replace("\"", "\'")
 
 
-def ask_all_questions(config, questions : list, output_file_path : str) -> None:
+def ask_all_questions(config, questions : list, output_file_path : str, verbose:bool) -> None:
+    if verbose: print("[sleepyask] Logging into ChatGPT with user credentials")
     chatbot = Chatbot(config=config)
+    if verbose: print("[sleepyask] Successfully logged in")
 
     def ask_chat_gpt(question: str) -> str:
         question = str(question)
-        message = ""
-        prev_text = ""
-        for data in chatbot.ask(question):
-            message += data["message"][len(prev_text):]
-            prev_text = data["message"]
+        
+        if verbose: print("[sleepyask] Asking:", question)
+
+        logging.disable(logging.ERROR)
+        try:
+            message = ""
+            # prev_text = ""
+            for data in chatbot.ask(question):
+                message = data["message"]
+        except json.decoder.JSONDecodeError: pass
+        logging.disable(logging.NOTSET)
+        
+        if verbose: print("[sleepyask] Received:", message)
         return message
     
     last_index = -1
@@ -34,5 +46,8 @@ def ask_all_questions(config, questions : list, output_file_path : str) -> None:
         if index <= last_index: continue
 
         chatgpt_response = ask_chat_gpt(question)
-        row_to_append = {"question_number": index, "question": question,"response": __clean_str_for_json(chatgpt_response)}
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+        row_to_append = {"date_time": dt_string, "question_number": index, "question": question,"response": __clean_str_for_json(chatgpt_response)}
         __append_to_file(output_file_path, row_to_append)
