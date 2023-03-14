e148344b4b69dc9fe87130e7c3010cb74147d971
hwelsters <84760072+hwelsters@users.noreply.github.com>
2023-03-14 13:17:23 -0700

    Log model as well

--- src/sleepyask/openai/chatgpt.py
+++ src/sleepyask/openai/chatgpt.py
@@ -14,7 +14,7 @@ def __append_to_file(output_file_path: str, data):
 def __clean_str_for_json(text: str):
     return text.replace("\"", "\'")
 
-def ask_questions(configs, questions : list, output_file_path: str, verbose: bool) -> None:
+def ask_questions(configs, questions : list, output_file_path: str, verbose: bool, model) -> None:
     question_queue = queue.Queue()
 
     def loader_worker():
@@ -24,8 +24,12 @@ def ask_questions(configs, questions : list, output_file_path: str, verbose: boo
         if Path(output_file_path).is_file():
             with open(output_file_path) as f:
                 asked_questions = []
+                index = 0
                 for line in f:
-                    asked_questions.append(json.loads(line))
+                    try:
+                        asked_questions.append(json.loads(line))
+                    except: print(f"Output file is incorrectly formatted {index}")
+                    index = index + 1
                 max_index = 0
 
                 for question in asked_questions:
@@ -49,25 +53,20 @@ def ask_questions(configs, questions : list, output_file_path: str, verbose: boo
         
         while True:
             question = question_queue.get()
-            
             message = ''
             if verbose: print(f"[sleepyask {index}] Asking:", question["question"])
             # logging.disable(logging.ERROR)
             message = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
+                model=model,
                 messages=[
                     {"role": "user", "content": question["question"]},
                 ]
             )["choices"][0]["message"]["content"]
-            # logging.disable(logging.NOTSET)
-            # if verbose: print(f"[sleepyask {index}] Failed to ask questions. Will try again in 15 minutes")
-            # question_queue.put(question)
-            # return
 
             if verbose: print(f"[sleepyask {index}] Received:", message)
 
             dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-            row_to_append = {"date_time": dt_string, "question_number": question["question_number"], "question": question["question"],"response": __clean_str_for_json(message)}
+            row_to_append = {"model": model, "date_time": dt_string, "question_number": question["question_number"], "question": question["question"],"response": __clean_str_for_json(message)}
             __append_to_file(output_file_path, row_to_append)
             question_queue.task_done()
 
