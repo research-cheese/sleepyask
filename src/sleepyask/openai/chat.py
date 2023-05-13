a8323611f88d02c9bda251e4fde8f7c158fdd1ce
hwelsters <84760072+hwelsters@users.noreply.github.com>
2023-05-12 19:42:07 -0700

    Update chat.py

--- src/sleepyask/openai/chat.py
+++ src/sleepyask/openai/chat.py
@@ -1,8 +1,9 @@
 from sleepyask.openai.chatgpt import ask_questions
 import traceback
 import logging
+import time
 
-def ask(config, questions : list, output_file_path : str, verbose: bool = False, model : str = "gpt-3.5-turbo", system_text : str | None = None, temperature : float | None = 1, max_tokens : int | None = 2048):
+def ask(configs : list, questions : list, output_file_path : str, verbose: bool = False, model : str = "gpt-3.5-turbo"):
     '''
     `config` should be a list containing your ChatGPT access tokens / email and password\n
     `questions` should contain a list of questions you would like ChatGPT to answer.\n
@@ -12,6 +13,9 @@ def ask(config, questions : list, output_file_path : str, verbose: bool = False,
     while redo:
         if not isinstance(questions, list): raise ValueError("[questions] should be a list")
 
-        try: ask_questions(config=config, questions=questions, output_file_path=output_file_path, verbose=verbose, model=model, system_text=system_text, temperature=temperature, max_tokens=max_tokens)
+        try: ask_questions(configs=configs, questions=questions, output_file_path=output_file_path, verbose=verbose, model=model)
         except Exception as e: 
             logging.error(traceback.format_exc())
+        
+        time.sleep(60)
+        
