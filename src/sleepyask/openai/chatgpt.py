8d09f610c3bb9a6e794f38a11d5ef5b0ddcec139
hwelsters <84760072+hwelsters@users.noreply.github.com>
2023-05-12 19:41:54 -0700

    Update chatgpt.py

--- src/sleepyask/openai/chatgpt.py
+++ src/sleepyask/openai/chatgpt.py
@@ -3,27 +3,28 @@ from pathlib import Path
 from datetime import datetime
 import threading
 import queue
-import logging
+import logging 
 import traceback
-import time
-import openai
 
-def __append_to_file(output_file_path: str, data):
-    with open(output_file_path, 'a') as outfile:
-        outfile.write(json.dumps(data))
-        outfile.write("\n")
-        outfile.close()
+
 
 
 def __clean_str_for_json(text: str):
-    return text
+    return text.replace("\"", "\'")
 
-def ask_questions(config, questions : list, output_file_path : str, verbose: bool = False, model : str = "gpt-3.5-turbo", system_text : str | None = None, temperature : float | None = 1, max_tokens : int | None = 2048) -> None:
+
+def ask_questions(configs, questions: list, output_file_path: str, verbose: bool, model) -> None:
     question_queue = queue.Queue()
     save_queue = queue.Queue()
 
-    openai.organization = config["organization"]
-    openai.api_key = config["api_key"]
+    def actually_append(output_file_path: str, data):
+        with open(output_file_path, 'a') as outfile:
+            outfile.write(json.dumps(data))
+            outfile.write("\n")
+            outfile.close()
+
+    def __append_to_file(output_file_path: str, data):
+        save_queue.put(data)
 
     def loader_worker():
         if verbose:
@@ -53,40 +54,28 @@ def ask_questions(config, questions : list, output_file_path : str, verbose: boo
 
         if question_queue.empty():
             print("[sleepyask] All questions exhausted")
-            print("\n***     DONE ASKING ALL QUESTIONS       ***")
+            print("""
+***     DONE ASKING ALL QUESTIONS       ***""")
 
-    def saver_worker():
-        while True:
-            if save_queue.empty(): continue
-            to_add = save_queue.get()
-            __append_to_file(output_file_path, to_add)
-            save_queue.task_done()
-
-    def asker_worker(index):
+    def asker_worker(index, config):
+        import openai
+        openai.organization = config["organization"]
+        openai.api_key = config["api_key"]
 
         succeed = True
         while succeed:
-            if question_queue.empty(): succeed = False
             question = question_queue.get()
             message = ''
             if verbose:
                 print(f"[sleepyask {index}] Asking:", question["question"])
+            # logging.disable(logging.ERROR)
             try:
-                messages = [
-                    {"role": "user", "content": question["question"]},
-                ]
-
-                if (system_text != None):
-                    messages.insert(
-                        0, {"role": "system", "content": system_text})
-
                 message = openai.ChatCompletion.create(
-                    temperature=temperature,
-                    max_tokens=max_tokens,
                     model=model,
-                    messages=messages
+                    messages=[
+                        {"role": "user", "content": question["question"]},
+                    ]
                 )
-
                 actual_model = message["model"]
                 usage = message["usage"]
                 message = message["choices"][0]["message"]["content"]
@@ -95,39 +84,39 @@ def ask_questions(config, questions : list, output_file_path : str, verbose: boo
                     print(f"[sleepyask {index}] Received:", message)
 
                 dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-                row_to_append = {"model": actual_model, "temperature": temperature, "max_tokens": max_tokens, "date_time": dt_string, "question_number":
-                                 question["question_number"], "question": question["question"], "system_text": str(system_text), "response": __clean_str_for_json(message), **usage}
-                save_queue.put(row_to_append)
+                row_to_append = {"model": actual_model, "date_time": dt_string, "question_number":
+                                 question["question_number"], "question": question["question"], "response": __clean_str_for_json(message), **usage}
+                __append_to_file(output_file_path, row_to_append)
 
-            except KeyboardInterrupt:
-                succeed = False
-                break
-            except openai.error.AuthenticationError:
-                logging.error(traceback.format_exc())
-                question_queue.put(question)
-                time.sleep(120)
             except:
+                logging.error(traceback.format_exc())
+                
                 question_queue.put(question)
-                time.sleep(120)
+                succeed = False
 
             question_queue.task_done()
 
-    thread_refs = []
-    for num in range(config["count"]):
-        t = threading.Thread(target=asker_worker, daemon=True, kwargs={'index': num})
-        thread_refs.append(t)
-        t.start()
+    def saver_worker():
+        while True:
+            to_append = save_queue.get()
+            actually_append(output_file_path, to_append)
 
-    loader = threading.Thread(target=loader_worker, daemon=True)
-    saver = threading.Thread(target=saver_worker, daemon=True)
-    thread_refs.append(loader)
-    thread_refs.append(saver)
-    
-    loader.start()
-    saver.start()
+    thread_refs = []
+    for index, config in enumerate(configs):
+        for num in range(config["count"]):
+            t = threading.Thread(target=asker_worker, daemon=True, kwargs={
+                'index': index * config["count"] + num, 'config': config})
+            thread_refs.append(t)
+            t.start()
+
+    t = threading.Thread(target=loader_worker, daemon=True)
+    thread_refs.append(t)
+    b = threading.Thread(target=saver_worker, daemon=True)
+    thread_refs.append(b)
+    t.start()
+    b.start()
 
     question_queue.join()
-    save_queue.join()
 
     for t in thread_refs:
         t.join()
