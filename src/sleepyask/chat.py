e866f9d19cdd94d7d862a9ac5d021496bdaf15e5
hwelsters <corevalue.noel@gmail.com>
2023-05-21 01:02:40 -0700

    Verbose mode

--- src/sleepyask/chat.py
+++ src/sleepyask/chat.py
@@ -12,7 +12,7 @@ class Sleepyask:
     This allows users to aggregate a large number of responses from ChatGPT.
     """
 
-    def __init__(self, configs : dict ={}, rate_limit: int = 5, api_key: str = "", timeout: int = 100, out_path : str = ""):
+    def __init__(self, configs : dict ={}, rate_limit: int = 5, api_key: str = "", timeout: int = 100, out_path : str = "", verbose: bool = False):
         """
         `configs` a dict containing parameters which will be part of the payload such as model, temperature, etc
         `rate_limit` the maximum number of questions you would like to ask a minute.
@@ -25,6 +25,7 @@ class Sleepyask:
         self.api_key = api_key
         self.timeout = timeout
         self.out_path = out_path
+        self.verbose = verbose
 
     def start(self, question_list):
         """
@@ -38,8 +39,14 @@ class Sleepyask:
         self.question_queue = queue.Queue()
         for question in question_list: self.question_queue.put(question)
 
+        question_index = 0
+
         while self.succeed < len(question_list):
-            tasks = [self.async_ask(self.question_queue.get()) for _ in range(self.rate_limit)]
+            tasks = []
+            for _ in range(self.rate_limit):
+                tasks.append(self.async_ask(self.question_queue.get(), question_index))
+                question_index += 1 
+
             await asyncio.gather(*tasks)
 
             if self.succeed < len(question_list): time.sleep(60)
@@ -50,15 +57,22 @@ class Sleepyask:
         with open(self.out_path, "a") as outfile:
             outfile.write(json.dump(response))
             outfile.write("\n")
+
         self.succeed += 1
         self.file_lock.release()
 
-    async def async_ask(self, question):
+    async def async_ask(self, question, question_index):
         try:
+            if self.verbose: print(f"[sleepyask] INFO | ID {question_index} | ASKING: {question}")
+
             payload = {"messages": [{"role": "user", "content": question}], **self.configs}
             response = await openai_async.chat_complete(payload=payload, api_key=self.api_key, timeout=self.timeout)
             
-            if response.status_code != 200: raise ValueError("Should be 200")
+            if response.status_code != 200: 
+                if self.verbose: print(f"[sleepyask] INFO | ID {question_index} | {response.status_code}")
+                raise ValueError("Should be 200")
 
             await self.log(response.text)
-        except: self.question_queue.put(question)
+        except: 
+            if self.verbose: print(f"[sleepyask] INFO | ID {question_index} | ERROR")
+            self.question_queue.put(question)
